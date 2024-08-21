d9ff975628039c9b54d2a230913fe8926505f952
Zahreddine Atoui <zahreddine@bumbal.eu>
2024-08-21 14:39:16 +0200

    chore: linting and formatting

--- src/web.ts
+++ src/web.ts
@@ -2,7 +2,8 @@ import { WebPlugin } from '@capacitor/core';
 
 import type {
   CapacitorMapboxNavigationPlugin,
-  MapboxNavOptions, MapboxResult,
+  MapboxNavOptions,
+  MapboxResult,
 } from './definitions';
 
 export class CapacitorMapboxNavigationWeb
@@ -15,18 +16,34 @@ export class CapacitorMapboxNavigationWeb
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async show(_options: MapboxNavOptions): Promise<MapboxResult> {
-    throw { status: 'failure', type: 'not_supported', data: 'navigation not supported on web' };
+    throw {
+      status: 'failure',
+      type: 'not_supported',
+      data: 'navigation not supported on web',
+    };
   }
 
   async history(): Promise<any> {
-    throw { status: 'failure', type: 'not_supported', data: 'navigation not supported on web' };
+    throw {
+      status: 'failure',
+      type: 'not_supported',
+      data: 'navigation not supported on web',
+    };
   }
 
   async requestPermissions(): Promise<any> {
-    throw { status: 'failure', type: 'not_supported', data: 'navigation not supported on web' };
+    throw {
+      status: 'failure',
+      type: 'not_supported',
+      data: 'navigation not supported on web',
+    };
   }
 
   async checkPermissions(): Promise<any> {
-    throw { status: 'failure', type: 'not_supported', data: 'navigation not supported on web' };
+    throw {
+      status: 'failure',
+      type: 'not_supported',
+      data: 'navigation not supported on web',
+    };
   }
 }
